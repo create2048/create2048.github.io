f5a7c8b590ba3095cad65e0b1e454abcba279dbe
create2048 <refrigerator900@gmail.com>
2024-11-13 01:57:14 -0600

    Upload custom2048 files

---
+++ js/application.js
@@ -0,0 +1,13 @@
+// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(function () {
+  game_manager = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+});
+
+function handle_undo() {
+  game_manager.move(-1);
+}
+
+function setpack() {
+localStorage.setItem("pack", document.getElementById("pack").value); 
+location.reload();
+}
